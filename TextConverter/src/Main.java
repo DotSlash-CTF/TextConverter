19edfee757c2b2846363a25d318b46f402c27ba8
Andrew Turley <aturley123@gmail.com>
2016-10-14 13:38:34 -0400

    Added ability to get the format of input

--- TextConverter/src/Main.java
+++ TextConverter/src/Main.java
@@ -7,19 +7,34 @@ public class Main {
 	public static void main(String[] args) {
 		String text = "";
 		Scanner sc = new Scanner(System.in); // Create ability to read in user input
-		while (true) {
+		String choice = "";
+		boolean next = false;
+		Text input;
+		
+		while (next == false) {
 			System.out.print("Import from file or console? (f/c): "); // Print to console
 			sc = sc.useDelimiter("\n"); // Set the scanner to only end when the user hits return
-			String choice = sc.next(); // Create a new string with the user's input
+			choice = sc.next(); // Create a new string with the user's input
 			switch (choice) {
 				case "f": // If the user typed in "f", then...
 					text = getTextFromFile();
+					next = true;
 					break;
 				case "c":
 					text = getTextFromConsole();
+					next = true;
 					break;
 				default: // If the user messed up, then...
 					System.out.println("It is literally one letter. How did you mess up?");
+			}
+		}
+		next = false;
+		while (next == false) {
+			System.out.print("What format is this, hex, ascii, binary, decimal? (h/a/b/d): ");
+			choice = sc.next();
+			switch (choice) {
+				case "h":
+					
 			}
 		}
 	}
@@ -28,7 +43,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		sc = sc.useDelimiter("\n");
 		System.out.println("Type in full path of txt file: ");
-		filename = sc.next();
+		String filename = sc.next();
 		
 	    String content = null;
 	    File file = new File(filename); // Create the file from given filename
@@ -42,7 +57,12 @@ public class Main {
 	    } catch (IOException e) {
 	        e.printStackTrace();
 	    } finally {
-	        if(reader != null){reader.close();}
+	        try {
+	        	reader.close();
+	        }
+	        catch (IOException e) {
+	        	e.printStackTrace();
+	        }
 	    }
 	    return content;
 	}
